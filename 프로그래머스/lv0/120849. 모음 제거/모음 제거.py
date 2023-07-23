09795b7a324333d34e0a4213f0677dee07d0a849
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-07-23 13:19:52 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    
+    vowel = ['a','e','i','o','u']
+    answer = []
+    for i in my_string:
+        if i not in vowel:
+            answer.append(i)
+    return ''.join(answer)
