6014e14c0866061088cb20419f0a34b69c9ebebc
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-07-26 17:17:29 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+# 28235 코드마스터 2023
+word = input()
+if word == 'SONGDO':
+    print('HIGHSCHOOL')
+elif word == 'CODE':
+    print('MASTER')
+elif word =='2023':
+    print('0611')
+else:
+    print('CONTEST')
