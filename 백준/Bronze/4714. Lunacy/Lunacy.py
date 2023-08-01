51c4b8d88758eea40a3847e90d61875e239ee009
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-01 11:15:14 +0900

    [Bronze IV] Title: Lunacy, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4714. Lunacy/Lunacy.py
@@ -0,0 +1,7 @@
+#4714 Lunacy
+while True:
+    n = float(input())
+    if n > 0:
+        print(f'Objects weighing {n:.2f} on Earth will weigh {n*0.167:.2f} on the moon.')
+    else: 
+        break
