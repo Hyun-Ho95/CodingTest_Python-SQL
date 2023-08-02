0541f434599a3a6f457b2b37368895e9b5b721d4
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-02 13:27:04 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -15,18 +15,19 @@ def solution(polynomial):
             x_coef_list.append(1)
         else:
             x_coef_list.append(int(i[:-1]))
-            
-    if sum(x_coef_list) ==0:
-        return f'{sum(num_list)}'
-    elif sum(x_coef_list) ==1:
-        if sum(num_list) ==0:
-            return 'x'
+    
+    a = sum(x_coef_list) # x 계수의 합
+    b = sum(num_list) # 상수항의 합
+    
+    if a ==0: # 일차항의 계수가 0일 때 => 상수항만 출력
+        return f'{b}'
+    elif a ==1: # 일차항의 계수가 1일 때 
+        if b ==0:
+            return 'x' # 1x아닌 x로 출력
         else:
-            return f'x + {sum(num_list)}'
-    elif sum(num_list) == 0:
-        return f'{sum(x_coef_list)}x'
-    elif sum(x_coef_list) > 1:
-        if sum(num_list) ==0:
-            return f'{sum(x_coef_list)}x'
+            return f'x + {b}' # x + 상수항 
+    elif a > 1: # 일차항의 계수가 1보다 클 때
+        if b ==0:
+            return f'{a}x' # ax 형태로 출력
         else:
-            return f'{sum(x_coef_list)}x + {sum(num_list)}'
+            return f'{a}x + {b}' # ax + b 형태로 출력
