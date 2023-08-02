53859875d09bd866fd318353a2223441363f5854
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-02 14:18:48 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
@@ -3,4 +3,5 @@ a = int(input()) # 집 -> 학교
 b = int(input()) # 학교 -> PC방
 c = int(input()) # PC방 -> 학원
 d = int(input()) # 학원 -> 집
-print((a+b+c+d)//60), print((a+b+c+d)%60, end = '\n')
+print((a+b+c+d)//60)
+print((a+b+c+d)%60)
