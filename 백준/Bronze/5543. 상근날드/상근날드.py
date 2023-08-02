9c76d39c552e56c3be739ece11722bc29f22de52
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-02 13:47:14 +0900

    [Bronze IV] Title: 상근날드, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,8 @@
+# 5543 상근날드
+sangdeok = int(input())
+jungdeok = int(input())
+hadeok = int(input())
+cola = int(input())
+cider = int(input())
+
+print(min(sangdeok,jungdeok,hadeok)+min(cola,cider)-50)
