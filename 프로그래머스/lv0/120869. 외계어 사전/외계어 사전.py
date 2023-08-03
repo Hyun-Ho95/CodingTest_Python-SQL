e2312b41a89894274971ecc864a40d9dde3fdba1
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-03 16:16:38 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -0,0 +1,10 @@
+def solution(spell,dic):
+    for i in dic:
+        cnt = 0
+        for j in spell:
+            if j in i:
+                cnt += 1
+                if cnt ==len(spell):
+                    return 1 
+                    break
+    return 2
