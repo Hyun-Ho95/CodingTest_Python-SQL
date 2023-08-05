e5244fbe0a6f10aaf1e31778014510ec5fabc251
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-05 11:07:36 +0900

    Update 유한소수 판별하기.py

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -4,24 +4,20 @@ def gcd(a, b):
     else:
         return gcd(b, a%b)
 
-def factorization(x):
-    d = 2
-    output = []
-    
-    while d <= x:
-        if x % d == 0:
-            output.append(d)
-            x /= d
-        else:
-            d += 1
-            
-    return output
-    
 def solution(a, b):
-    divide_num = gcd(b, a)
-    result = b // divide_num
     
-    for num in factorization(result):
-        if num not in [2, 5]:
+    b = b // gcd(a, b)
+    
+    result = []
+    start_num = 2
+    while start_num <=b:
+        if b % start_num==0:
+            result.append(start_num)
+            b = b//start_num
+        else:
+            start_num += 1
+        
+    for i in result:
+        if i not in [2,5]:
             return 2
     return 1
