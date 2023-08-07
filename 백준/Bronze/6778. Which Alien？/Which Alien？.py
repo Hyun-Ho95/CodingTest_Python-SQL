d72719fd68721f42ba02b31cfd9722e1f41f0372
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-07 17:43:39 +0900

    Update Which Alien？.py

--- 백준/Bronze/6778. Which Alien？/Which Alien？.py
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -1,4 +1,4 @@
-
+# 6778 Which Alien?
 antenna = int(input())
 eyes = int(input())
 
@@ -9,3 +9,6 @@ if antenna <= 6 and eyes >= 2:
     print("VladSaturnian")
 if antenna <= 2 and eyes <= 3:
     print("GraemeMercurian")
+
+# 여기서 중요한 점은 elif로 하면 두가지 이상의 경우에 해당되는 외계인(ex antenna = 2 , eyes = 3)은 출력되지 않는다
+# if를 3줄로 작성해주어야함
