699c6c6b9eccdc2123eaab197010b115dfc14b21
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-07 10:50:26 +0900

    Update 옹알이 （1）.py

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,7 +1,16 @@
 def solution(babbling):
-    word_list = ['aya', 'ye', 'woo', 'ma', 'ayaye', 'ayawoo', 'ayama', 'yeaya', 'yewoo', 'yema', 'wooaya', 'wooye', 'wooma', 'maaya', 'maye', 'mawoo', 'ayayewoo', 'ayayema', 'ayawooye', 'ayawooma', 'ayamaye', 'ayamawoo', 'yeayawoo', 'yeayama', 'yewooaya', 'yewooma', 'yemaaya', 'yemawoo', 'wooayaye', 'wooayama', 'wooyeaya', 'wooyema', 'woomaaya', 'woomaye', 'maayaye', 'maayawoo', 'mayeaya', 'mayewoo', 'mawooaya', 'mawooye', 'ayayewooma', 'ayayemawoo', 'ayawooyema', 'ayawoomaye', 'ayamayewoo', 'ayamawooye', 'yeayawooma', 'yeayamawoo', 'yewooayama', 'yewoomaaya', 'yemaayawoo', 'yemawooaya', 'wooayayema', 'wooayamaye', 'wooyeayama', 'wooyemaaya', 'woomaayaye', 'woomayeaya', 'maayayewoo', 'maayawooye', 'mayeayawoo', 'mayewooaya', 'mawooayaye', 'mawooyeaya']
-    cnt = 0
-    for i in babbling:
-        if i in word_list:
-            cnt+=1
-    return cnt
+    answer = []
+    count = 0
+    for n in babbling:
+        n = n.replace('aya' , '*')
+        n = n.replace('ye', '*')
+        n = n.replace('woo' , '*')
+        n = n.replace('ma', '*')
+        answer.append(str(set(n)))
+    
+    for a in answer:
+        if "{'*'}"== a:
+            count += 1
+        else:
+            continue
+    return count
