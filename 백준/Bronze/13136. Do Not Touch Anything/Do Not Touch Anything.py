275a55fed2d880abc9a34e4b88141c8503581c6e
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-08-30 08:21:23 +0900

    [Bronze IV] Title: Do Not Touch Anything, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.py
@@ -0,0 +1,12 @@
+# 13136 Do Not Touch Anything
+r, c, n = map(int,input().split())
+if r % n ==0:
+    x = r // n
+else:
+    x = (r // n) +1
+    
+if c % n == 0:
+    y = c // n
+else:
+    y = (c // n) +1
+print(x*y)
