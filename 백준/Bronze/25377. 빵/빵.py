3581e43c3bee42a58a36a71f0fe36e8b60ffa6fd
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-09-30 11:01:43 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 31256 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.py
@@ -0,0 +1,14 @@
+# 25377 빵
+n = int(input())
+cnt = 0
+sol = []
+for _ in range(n):
+    a,b = map(int,input().split())
+    if a >= b:
+        cnt +=1
+    else:
+        sol.append(b)
+if cnt ==n:
+    print(-1)
+else:
+    print(min(sol))
