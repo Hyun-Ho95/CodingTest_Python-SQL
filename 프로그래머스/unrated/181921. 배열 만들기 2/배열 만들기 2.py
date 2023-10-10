ad1200cbefe17ab7c0eb58b962074ccb4793ea27
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-10-10 16:32:12 +0900

    [level unrated] Title: 배열 만들기 2, Time: 27.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,6 +1,6 @@
 def solution(l, r):
     answer = []
     for num in range(l, r+1):
-        if not set(str(num)) - {'0','5'}:
+        if set(str(num)) - {'0','5'} == set():
             answer.append(num)
     return answer if answer else [-1]
