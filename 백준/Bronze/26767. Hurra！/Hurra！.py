04d4b1361394eeb6921b3bd1097cff91d25d134d
Its me HyunHo <62702596+Hyun-Ho95@users.noreply.github.com>
2023-10-16 11:37:41 +0900

    [Bronze IV] Title: Hurra!, Time: 708 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26767. Hurra！/Hurra！.py
@@ -0,0 +1,11 @@
+# 26767 Hurra!(Hurra 게임)
+n = int(input())
+for i in range(1,n+1):
+    if i%7==0 and i%11==0:
+        print('Wiwat!')
+    elif i%7!=0 and i%11==0:
+        print('Super!')
+    elif i%7==0 and i%11!=0:
+        print('Hurra!')
+    else:
+        print(i)
