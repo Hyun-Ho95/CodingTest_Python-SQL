720eee1f7509181d564e360fb557fa7134873de7
HyunHo PARK <62702596+Hyun-Ho95@users.noreply.github.com>
2023-10-26 11:30:27 +0900

    Update A 강조하기.py

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,7 +1,11 @@
 def solution(myString):
-    for idx,k in enumerate(myString):
-        if k =='a':
-            myString = myString.replace(myString[idx],'A')
+    myString = list(myString)  # 문자열을 리스트로 변환
+    
+    for idx, k in enumerate(myString):
+        if k == 'a':
+            myString[idx] = 'A'
         elif k != 'A' and k.isupper():
-            myString = myString.replace(myString[idx], k.lower())
+            myString[idx] = k.lower()
+    
+    myString = ''.join(myString)  # 리스트를 문자열로 변환
     return myString
