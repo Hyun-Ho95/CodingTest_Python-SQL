650e9d1f5339b2efc9fe377809dee1187d5fbaca
HyunHo PARK <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-01 13:26:45 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+# 30402 감마선을 맞은 컴퓨터
+for _ in range(15):
+    cat_list = list(input().split())
+    if 'w' in cat_list:
+        print('chunbae')
+        break
+    elif 'b' in cat_list:
+        print('nabi')
+        break
+    elif 'g' in cat_list:
+        print('yeongcheol')
+        break
