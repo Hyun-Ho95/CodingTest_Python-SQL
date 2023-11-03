de5418054540275701cf4d32504c2d4d14885788
HyunHo PARK <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-03 10:50:33 +0900

    [Bronze III] Title: 플러그, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.py
+++ 백준/Bronze/2010. 플러그/플러그.py
@@ -1,11 +1,9 @@
 # 2010번 플러그
 import sys
-input = sys.stdin.readline
 
 cnt = 0 
 n = int(input())
 
 for _ in range(n):
-    cnt += int(input())
-    
+    cnt += int(sys.stdin.readline())
 print(cnt - (n-1)*1)
