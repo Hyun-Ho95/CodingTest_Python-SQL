52c2a8b163ca12ca0d7f63053e03d513d885b81b
HyunHo PARK <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-05 18:24:34 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -0,0 +1,11 @@
+# 2455번 지능형 기차 
+result = []
+cnt = 0
+
+for _ in range(10):
+    a,b = map(int,input().split())
+    result.append(cnt-a)
+    cnt -= a
+    result.append(cnt+b)
+    cnt += b
+print(max(result))
