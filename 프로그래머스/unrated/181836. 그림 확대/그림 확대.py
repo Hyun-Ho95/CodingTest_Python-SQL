c367835a07e0cadfc4ab3f103c5e9399013e023d
HyunHo PARK <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-08 20:32:32 +0900

    [level unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -0,0 +1,11 @@
+def solution(picture, k):
+    answer = []
+    
+    for i in picture:
+        string = ''
+        for j in i:
+            string += j*k
+        for _ in range(k):
+            answer.append(string)
+        
+    return answer
