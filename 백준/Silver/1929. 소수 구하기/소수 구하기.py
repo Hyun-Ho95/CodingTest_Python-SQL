82423277c350b39182b49a7c0d3df0a921780e69
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2023-11-28 11:51:16 +0900

    [Silver III] Title: 소수 구하기, Time: 5796 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,10 @@
+# 1929번 소수 구하기
+n,m = map(int,input().split())
+result = []
+for i in range(n,m+1):
+    if i >1:
+        for j in range(2,int(i**0.5)+1):
+            if i % j==0:
+                break
+        else:
+            print(i)
