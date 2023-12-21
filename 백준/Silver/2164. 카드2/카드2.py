e35f4b17ec00c7dfbaff4ea46382013d130731d1
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2023-12-21 14:16:09 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 50932 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,12 @@
+# 2164 카드2(Silver IV)
+from collections import deque
+n = int(input())
+deq = deque()
+
+for i in range(1,n+1):
+    deq.append(i)
+
+while len(deq)>1:
+    deq.popleft()
+    deq.rotate(-1)
+print(list(deq)[0])
