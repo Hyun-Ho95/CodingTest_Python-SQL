674e0d967ae5f191e45cb5d434311aa3a037c730
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2023-12-31 13:59:49 +0900

    [Bronze II] Title: 분산처리, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,21 @@
+# 1009번 분산처리
+import sys
+t = int(input())
+answer =[]
+
+for _ in range(t):
+    a,b = map(int,input().split())
+    b %= 4
+    
+    if b%4==0:
+        b=4
+    
+    s = a**b
+    
+    if s%10 ==0:
+        answer.append(10)
+    else:
+        answer.append(s%10)
+
+for i in answer:
+    print(i)
