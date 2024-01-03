00355fb3df33c8454b8eed603fd0408e58d643bb
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-03 20:54:56 +0900

    Update 이상한 곱셈.py

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
@@ -1,12 +1,12 @@
 # 1225번 이상한 곱셈 (Bronze II)
 a,b = input().split()
-arr = []
-for i in range(len(a)):
-    arr.append(int(a[i]))
 
-ans = sum(arr)
 cnt = 0
+for i in a:
+    cnt += int(i)
 
-for j in range(len(b)):
-    cnt += ans*int(b[j])
-print(cnt)
+ans = 0
+for j in b:
+    ans += cnt*int(j)
+
+print(ans)
