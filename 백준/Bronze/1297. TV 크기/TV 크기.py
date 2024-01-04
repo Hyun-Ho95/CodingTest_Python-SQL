f5496c7bafc442db56cd6e0c4d2025fe6f9bcfe2
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-04 11:29:22 +0900

    [Bronze II] Title: TV 크기, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1297. TV 크기/TV 크기.py
+++ 백준/Bronze/1297. TV 크기/TV 크기.py
@@ -1,7 +1,8 @@
 # 1297번 TV 크기(Bronze II)
 import math
 d,h,w = map(int,input().split())
-y = (w*d)/math.sqrt((h**2)+(w**2))
-x = (h*d)/math.sqrt((h**2)+(w**2))
-# x = math.floor((h*y)/w)
-print(int(x),int(y))
+# TV 너비(가로) : x / TV 높이(세로) : y
+x = (w*d)/math.sqrt((h**2)+(w**2))
+y = (h*d)/math.sqrt((h**2)+(w**2))
+# y = (h*x)/w
+print(int(y),int(x))
