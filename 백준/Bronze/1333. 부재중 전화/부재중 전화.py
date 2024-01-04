f75c4df3c67ec10341a456587f8cb9dc5050c41e
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-04 17:47:35 +0900

    [Bronze II] Title: 부재중 전화, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -0,0 +1,13 @@
+## 1333번 부재중 전화(Bronze II)
+n,l,d = map(int,input().split())
+song = [False] * ((n*l)+(n-1)*5) # 앨범 총 길이
+len(song)
+for i in range(0,len(song),l+5): # 노래 시작시간 
+    for j in range(i,i+l): # 노래가 시작되고 재생되는 시간
+        song[j] = True
+song
+for k in range(0,len(song),d):
+    if song[k]==False:
+        print(k)
+        break
+else: print(k+d)
