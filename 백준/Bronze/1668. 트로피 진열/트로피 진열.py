8ec075244d8c7076303a74df85c308ca0fe845a3
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-08 16:33:23 +0900

    Update 트로피 진열.py

--- 백준/Bronze/1668. 트로피 진열/트로피 진열.py
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.py
@@ -1,22 +1,40 @@
 # 1668번 트로피 진열(Bronze II)
+from collections import deque
+
 arr = []
+cnt_l = 0
+cnt_r = 0
 
 for _ in range(int(input())):
     arr.append(int(input()))
     
-# 왼쪽 방향
-max_lh = cnt_l = 0
-for i in arr:
-    if max_lh < i:
-        max_lh = i
-        cnt_l += 1
+left = deque(arr[:arr.index(max(arr))+1])
+right = deque((arr[arr.index(max(arr)):]))
 
-# 오른쪽 방향
-max_rh = cnt_r = 0
-for j in range(len(arr)-1,-1,-1):
-    if max_rh < arr[j]:
-        max_rh = arr[j]
+# 왼쪽 시점
+max_left = 0
+while True:
+    if left == deque([]):
+        break
+    
+    if left[0] > max_left:
+        cnt_l += 1
+        max_left = left[0]
+        left.popleft()
+    else:
+        left.popleft()
+    
+# 오른쪽 시점    
+max_right = 0
+while True:
+    if right == deque([]):
+        break
+
+    if right[-1] > max_right :
         cnt_r += 1
-        
+        max_right = right[-1]
+        right.pop()
+    else:
+        right.pop()     
 print(cnt_l)
 print(cnt_r)
