3647a37b567e8ca85a8c6e47ac53336785532510
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-12 17:54:52 +0900

    [Bronze II] Title: 문어 숫자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.py
@@ -0,0 +1,12 @@
+# 1864번 문어 숫자(BronzeII)
+word = {'-':0, "\\":1, '(':2, '@':3, '?':4
+      , '>':5, '&':6, '%':7, '/': -1}
+while True:
+    num = input()
+    ans = 0
+    if num == '#':
+        break
+    
+    for i in range(len(num)):
+        ans += word[num[i]] * 8 ** (len(num)-i-1)
+    print(ans)
