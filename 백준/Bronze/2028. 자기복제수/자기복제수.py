63598193abeb3577f305aa48b5508b652a65cc0a
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-01-20 16:33:11 +0900

    [Bronze II] Title: 자기복제수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2028. 자기복제수/자기복제수.py
@@ -0,0 +1,7 @@
+# 2028번 자기복제수(BronzeII)
+for _ in range(int(input())):
+    num = int(input())
+    if str(num) == str(num**2)[-len(str(num)):]:
+        print('YES')
+    else:
+        print('NO')
