f1c443c697d45fe2ece38fbddcbb530d04fabbf4
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-02-01 10:05:07 +0900

    Update 국회의원 선거.py

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -11,11 +11,15 @@ for i in range(n):
     if n==1:
         print(0)
         break
+        
+    # 처음 후보자 다솜은 변수에 할당 후 나머지는 후보자 리스트에 넣기    
     else:
         if i == 0:
             dasom = x
         else:
             candidates.append(x)
+
+# 위 for문에서 break를 만나지 않았을 때 (후보자가 다솜 한명이 아닌 경우)
 else:
     while True:
         if dasom > max(candidates):
