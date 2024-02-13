0d8d66ef43ef61646facdd64e5feffa7b0bcc212
IT's me HYUNHO <62702596+Hyun-Ho95@users.noreply.github.com>
2024-02-13 16:39:57 +0900

    Update 입양 시각 구하기（2）.sql

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -6,17 +6,3 @@ SELECT (@HOUR := @HOUR+1) AS HOUR # 대입 연산자
 FROM ANIMAL_OUTS
 WHERE @HOUR < 23
 ORDER BY HOUR
-
-
-
-
-
-
-
-# SET @HOUR := -1 ; # 변수 선언문
-# SELECT (@HOUR := @HOUR +1) AS HOUR # := 대입연산자
-#      , (SELECT COUNT(DATETIME) 
-#         FROM ANIMAL_OUTS 
-#         WHERE HOUR(DATETIME) = @HOUR) AS COUNT
-# FROM ANIMAL_OUTS
-# WHERE @HOUR < 23
